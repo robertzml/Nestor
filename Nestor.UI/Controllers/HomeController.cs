b6646d58252e1137ad91878c23a40be28e367a7a
Robert Home <robertzml@126.com>
2014-08-21 23:40:59 +0800

    Edit layout page.
    Signed-off-by: Robert Home <robertzml@126.com>

--- Nestor.UI/Controllers/HomeController.cs
+++ Nestor.UI/Controllers/HomeController.cs
@@ -8,23 +8,21 @@ namespace Nestor.UI.Controllers
 {
     public class HomeController : Controller
     {
+        #region Action
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        /// <summary>
+        /// 菜单
+        /// </summary>
+        /// <returns></returns>
+        [ChildActionOnly]
+        public ActionResult Menu()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        #endregion //Action
     }
 }
