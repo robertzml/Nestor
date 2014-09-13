92c4521adf71fdea9232e09046591e7c51e3256a
Robert Home <robertzml@126.com>
2014-09-14 00:34:23 +0800

    Add admin area.
    Signed-off-by: Robert Home <robertzml@126.com>

---
+++ Nestor.UI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nestor.UI.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// 后台主页控制器
+    /// </summary>
+    public class HomeController : Controller
+    {
+        #region Action
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 菜单
+        /// </summary>
+        /// <returns></returns>
+        [ChildActionOnly]
+        public ActionResult Menu()
+        {
+            return View();
+        }
+        #endregion //Action
+    }
+}
