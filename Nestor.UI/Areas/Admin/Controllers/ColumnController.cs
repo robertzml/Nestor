f27b62c80d4d650a165dfa44e3989018f9025730
Robert Lab <robertzml@126.com>
2014-09-14 21:29:08 +0800

    Add Nestor.Business project. Add Nestor.Data project.
    Signed-off-by: Robert Lab <robertzml@126.com>

---
+++ Nestor.UI/Areas/Admin/Controllers/ColumnController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nestor.UI.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// 栏目管理控制器
+    /// </summary>
+    public class ColumnController : Controller
+    {
+        #region Action
+        // GET: Admin/Column
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion //Action
+    }
+}
