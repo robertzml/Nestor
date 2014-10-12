c7cb263dca6b4b05e6f85a6bb55e6794152cbd45
Robert <robertzml@126.com>
2014-10-12 21:03:28 +0800

    Fix some issue.
    Signed-off-by: Robert <robertzml@126.com>

--- Nestor.UI/App_Start/RouteConfig.cs
+++ Nestor.UI/App_Start/RouteConfig.cs
@@ -28,6 +28,13 @@ namespace Nestor.UI
             );
 
             routes.MapRoute(
+                name: "Search",
+                url: "search/t={t}",
+                defaults: new { controller = "Search", action = "Index" },
+                namespaces: new[] { "Nestor.UI.Controllers" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
