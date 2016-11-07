4734e9ba8c14847f2e41f68c1e8edc6a5abbb621
robertzml <robertzml@126.com>
2016-11-07 20:03:30 +0800

    Edit Creative home view.
    Signed-off-by: robertzml <robertzml@126.com>

--- Nestor.UI/Areas/Creative/Controllers/HomeController.cs
+++ Nestor.UI/Areas/Creative/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Nestor.Business;
 
 namespace Nestor.UI.Areas.Creative.Controllers
 {
@@ -19,6 +20,26 @@ namespace Nestor.UI.Areas.Creative.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 主页最新
+        /// </summary>
+        /// <param name="columnId"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        [ChildActionOnly]
+        public ActionResult HomeRecent(int columnId, int count)
+        {
+            ColumnBusiness columnBusiness = new ColumnBusiness();
+            var column = columnBusiness.Get(columnId);
+
+            ViewBag.Title = column.Title;
+
+            ArticleBusiness articleBusiness = new ArticleBusiness();
+
+            var data = articleBusiness.GetRecent(columnId, count);
+            return View(data);
         }
         #endregion //Action
     }
